bf9631dedaec833e56323a0675d61df0d96f969a
HY Devinton <user@HY-Devinton.local>
2025-12-18 12:50:51 -0800

    hero background change

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -6,33 +6,11 @@ import { Link } from 'react-router-dom';
 const HeroSection = () => (
   <div className="hero-section">
     <div className="cont m-0 p-0 d-flex justify-content-center align-items-center flex-column text-white">
-      <h1 style={{fontSize: '5rem'}} className="display-4 fw-bold px-5 text-center">2025: My Covenant Year of PURSUING INTIMACY WITH GOD</h1>
-      {/* <div className="row p-5 m-0">
-        <div className="col-lg-6">
-          <h4 className="text-center mb-4">James 4:8 (NKJV)</h4>
-          <p className="lead mb-4 text-justify">
-            8 Draw near to God and He will draw near to you. Cleanse your hands, you sinners; and purify your hearts, you double-minded.
-          </p>
-        </div>
-        <div className="col-lg-6">
-          <h4 className="text-center mb-4">James 4:8 (AMP)</h4>
-          <p className="lead mb-4 text-justify">
-
-            8 Draw near to God and He will draw near to you. Wash your hands, you sinners; purify your hearts, you hypocrites.
-          </p>
-        </div>
-        <div className="col-lg-12">
-          <h4 className="text-center mb-4">James 4:8 (NLT)</h4>
-          <p className="lead mb-4 text-justify">
-            Come close to God and He will come close to you. [Recognize that you are] sinners, get your soiled hands clean; [realize that you have been disloyal] wavering individuals with divided interests, and purity your hearts [of your spiritual adultery].
-          </p>
-          
-        </div>
-      </div> */}
+      <h1 style={{fontSize: '5rem'}} className="display-4 fw-bold px-5 text-center text-dark">2025: My Covenant Year of PURSUING INTIMACY WITH GOD</h1>
 
       <div className="d-flex justify-content-center gap-3">
-        <Link to='join-member' className="btn btn-light text-dark px-4">Join Now</Link>
-        <Link to='donation'><button className="btn btn-outline-light px-4">Make a Donation</button></Link>
+        <Link to='join-member' className="btn btn-dark text-white px-4 border">Join Now</Link>
+        <Link to='donation'><button className="btn btn-outline-dark px-4">Make a Donation</button></Link>
       </div>
     </div>
   </div>
